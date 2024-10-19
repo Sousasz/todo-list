51e4a7e812afd94cecf771f44034b21781e46eae
Sousa Morais <vi.morais@yahoo.com>
2024-10-19 16:55:05 -0300

    adjusted tasks content

--- src/components/TasksToDo/index.tsx
+++ src/components/TasksToDo/index.tsx
@@ -22,15 +22,15 @@ export default function TasksToDo({ allTasks, completeTask, deleteTask }: TasksT
             allTasks?.map((addedTasks: string) => {
               return(
                 <div key={addedTasks} className="bg-purple-1000 w-full max-[470px]:w-[85dvw] h-20 rounded-lg flex justify-between items-center p-6 mx-2">
-                  <span className="text-violet-400 truncate max-w-72">{addedTasks}</span>
+                  <span className="text-violet-400 truncate max-[465px]:max-w-[190px] max-[375px]:max-w-[140px] max-[310px]:max-w-[100px] max-w-[270px]">{addedTasks}</span>
 
-                  <div className="flex gap-4">
+                  <div className="flex gap-5">
                     <button onClick={() => completeTask(addedTasks)}>
                       <Check className="text-violet-500 w-7 h-7"/>
                     </button>
 
                     <button onClick={() => deleteTask(addedTasks)}>
-                      <Trash className="text-violet-500" />
+                      <Trash className="text-violet-500 " />
                     </button>
                   </div>
                 </div>
--- src/components/completedTasks/index.tsx
+++ src/components/completedTasks/index.tsx
@@ -13,7 +13,7 @@ export default function CompletedTasks({ tasksCompleted, deleteTaskDone }: Compl
             {tasksCompleted?.map((isTaskCompleted: string, index) => {
               return (
                 <div key={index} className="bg-purple-1000 w-full h-20 rounded-lg max-[470px]:w-[85dvw] flex justify-between items-center p-6">
-                  <s className="text-emerald-200 truncate max-w-80">{isTaskCompleted}</s>
+                  <s className="text-emerald-200 truncate max-[465px]:max-w-[250px] max-[375px]:max-w-[190px] max-[310px]:max-w-[120px] max-w-80">{isTaskCompleted}</s>
 
                   <div className="flex flex-row-reverse gap-4">
                     <button onClick={() => deleteTaskDone(isTaskCompleted)}>
